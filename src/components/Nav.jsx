392df01140f8fa9bc11ff44bc356472034d24402
aaade_pm <olatunjiademolaimmanuel@gmail.com>
2024-05-20 13:02:08 +0100

    styling and responsiveness overhaul

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,15 @@ import ContactButton from "./ContactButton";
 import boy from "../assets/boy.png";
 
 const Nav = () => {
-  const surname = "OLATUNJI E.";
+  const surname = "OLATUNJI E. ";
   const middlename = "ADEMOLA";
   const currentTime = new Date().toLocaleTimeString();
 
   return (
     <div className="nav">
       <div className="header-name">
-        <h1>{surname}</h1>
         <h1>
+          {surname}
           {middlename}
           <span>
             <img src={boy} alt="boy icon" />
@@ -18,8 +18,10 @@ const Nav = () => {
         </h1>
       </div>
       <div className="header-title">
-        <h3>LAGOS, NG</h3>
-        <h3>{currentTime}</h3>
+        <div className="header-date-time">
+          <h3>LAGOS, NG</h3>
+          <h3>{currentTime}</h3>
+        </div>
         <ContactButton />
       </div>
     </div>
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,37 +1,57 @@
 import Nav from "../components/Nav";
 
 const Hero = () => {
-  const desc1 = "FRONTEND";
-  const desc2 = "DEVELOPER";
+  const desc1 = "FRONT";
+  const desc2 = " —";
+  const desc3 = "END";
+  const desc4 = "DEVELOPER";
   const aboutMe1 = `🖐🏽 Hey there! I'm Ademola, a frontend developer passionate about creating stunning digital experiences.`;
   const aboutMe2 = `🚀 I'm a curious learner, always honing my skills and exploring ways to build efficiently.`;
   const aboutMe3 = `🌟 I'm currently open to new opportunities and excited to work on projects that challenge me and push me to grow.`;
 
   return (
-    <div className="hero">
-      <div className="scroll-watcher"></div>
-      <Nav />
-      <header className="header">
-        <div className="header-descriptions">
-          <div className="header-description">
-            {desc1.split("").map((letter, index) => {
-              return <h1 key={index}>{letter}</h1>;
-            })}
+    <>
+      <div className="hero">
+        <div className="scroll-watcher"></div>
+        <Nav />
+        <header className="header">
+          <div className="header-descriptions">
+            <div className="front">
+              <div className="header-description first">
+                {desc1.split("").map((letter, index) => {
+                  return <h1 key={index}>{letter}</h1>;
+                })}
+              </div>
+
+              <div className="end">
+                <div className="dash">
+                  {desc2.split("").map((letter, index) => {
+                    return <h1 key={index}>{letter}</h1>;
+                  })}
+                </div>
+                <div className="front-end">
+                  {desc3.split("").map((letter, index) => {
+                    return <h1 key={index}>{letter}</h1>;
+                  })}
+                </div>
+              </div>
+            </div>
+
+            <div className="header-description">
+              {desc4.split("").map((letter, index) => {
+                return <h1 key={index}>{letter}</h1>;
+              })}
+            </div>
           </div>
 
-          <div className="header-description">
-            {desc2.split("").map((letter, index) => {
-              return <h1 key={index}>{letter}</h1>;
-            })}
+          <div className="about">
+            <p>{aboutMe1}</p>
+            <p>{aboutMe2}</p>
+            <p>{aboutMe3}</p>
           </div>
-        </div>
-        <div className="about">
-          <p>{aboutMe1}</p>
-          <p>{aboutMe2}</p>
-          <p>{aboutMe3}</p>
-        </div>
-      </header>
-    </div>
+        </header>
+      </div>
+    </>
   );
 };
 
